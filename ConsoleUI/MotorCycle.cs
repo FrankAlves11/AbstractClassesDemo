3892efb11b34484ccfbc3c9aff6d6ad81721fe4f
FrankAlves11 <Frankie.alves@icloud.com>
2023-09-11 18:21:10 -0700

    made changes

---
+++ ConsoleUI/MotorCycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class MotorCycle : Vehicle
+    {
+        public bool HasSideCart { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("The motorcycle is driving while shifting gears..");
+        }
+
+        
+        
+            
+        
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,22 +33,73 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
-
+            var vehicles = new List<Vehicle>();
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * 
              * Set the properties with object initializer syntax
              */
+            var car1 = new Car()
+            {
+                HasTrunk = true,
+                Year = "2005",
+                Make = "BMW",
+                Model = "M4"
+
+            };
+
+
+            var motorcycle1 = new MotorCycle()
+            {
+                HasSideCart = true,
+                Year = "2010",
+                Make = "Kawasaki",
+                Model = "Ninja",
+
+            };
+
+            Vehicle vehicle1 = new Car()
+            {
+               
+                Year = "2023",
+                Make = "Ford",
+                Model = "Raptor"
+
+            };
+
+            Vehicle vehicle2 = new MotorCycle()
+            {
+                Year = "2015",
+                Make = "Harley",
+                Model = "Fatboy"
+
+            };
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
 
+            vehicles.Add(car1);
+            vehicles.Add(motorcycle1);
+            vehicles.Add(vehicle1);
+            vehicles.Add(vehicle2);
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"Year: {vehicle.Year}");
+                Console.WriteLine($"Make: {vehicle.Make}");
+                Console.WriteLine($"Model: {vehicle.Model}");
+                Console.WriteLine();
+                vehicle.DriveAbstract();
+                vehicle.DriveVirtual();
+                Console.WriteLine();
+            }
+
             // Call each of the drive methods for one car and one motorcycle
 
             #endregion            
-            Console.ReadLine();
+            
         }
     }
 }
